26d651d1356fbafe9e016a20a66d682c9536ae01
İsmail Daşcı <109577378+ismaildasci@users.noreply.github.com>
2024-01-01 13:16:03 +0300

    info modal component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,48 @@
+import React from 'react';
+import { NextPageContext } from 'next';
+import { getSession } from 'next-auth/react';
+
+import Navbar from '@/components/Navbar';
 import Billboard from '@/components/Billboard';
 import MovieList from '@/components/MovieList';
-import Navbar from '@/components/Navbar';
-import useFavorites from '@/hooks/useFavorites';
+import InfoModal from '@/components/InfoModal';
 import useMovieList from '@/hooks/useMovieList';
-import { NextPageContext } from 'next';
-import {getSession, signOut} from 'next-auth/react';
+import useFavorites from '@/hooks/useFavorites';
+import useInfoModal from '@/hooks/useInfoModal';
 
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
 
-
-export async function getServerSideProps(context:NextPageContext){
-    const session = await getSession(context);
-    if(!session)
-    {
-        return {
-            redirect:{
-            destination: '/auth',
-            permanent:false,
-            }
-        }
-    }
+  if (!session) {
     return {
-        props:{}
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
     }
+  }
+
+  return {
+    props: {}
+  }
 }
 
-export default function Home() {
+const Home = () => {
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  const {isOpen, closeModal} = useInfoModal();
 
-    const {data:movies = []} = useMovieList();
-    const {data: favorites = []} = useFavorites();
   return (
     <>
-    <Navbar />
-    <Billboard />
-  <div className='pb-40'>
-  <MovieList title="Trending Now" data={movies} />
-  <MovieList title="My List" data={favorites} />
-  </div>
+      <InfoModal visible={isOpen} onClose={closeModal} />
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
+      </div>
     </>
   )
 }
+
+export default Home;
